342f5eea77dae1092a80cd9177bc6d51a9c67676
Vanostra <109914394+vanostrax@users.noreply.github.com>
2023-01-31 13:07:10 +0700

    Update __init__.py

--- HiroshiX/__init__.py
+++ HiroshiX/__init__.py
@@ -1,8 +1,8 @@
 """
-  RiZoeLX 2022 © pyRiZoeLX
+  HiroshiX 2022 © pyHiroshiX
 """
 
 from .developers import *
-from RiZoeLX.functions import *
+from HiroshiX.functions import *
 
 __version__ = "1.0.9"
